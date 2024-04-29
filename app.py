97471e22a73352b85ae2ea6d27a5cdfd85ef3089
Jaimes Subroto <jaimessubroto@gmail.com>
2024-04-29 15:06:15 -0700

    Add edit

--- app.py
+++ app.py
@@ -53,12 +53,12 @@ def contacts_view(contact_id):
 
 
 @app.route("/contacts/<int:contact_id>/edit", methods=["GET"])
-def contacts_get(contact_id):
+def contacts_edit_get(contact_id):
     return render_template("edit.html", contact=Contact.get_by_id(contact_id))
 
 
 @app.route("/contacts/<int:contact_id>/edit", methods=["POST"])
-def contacts_post(contact_id):
+def contacts_edit_post(contact_id):
     c = Contact.get_by_id(contact_id)
     c.update(request.form["firstName"], request.form["lastName"], request.form["email"], request.form["phone"])
     if c.save():
